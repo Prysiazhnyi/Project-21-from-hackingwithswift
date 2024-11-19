432175dfdef8fcae05854134f0fdfbe8f27c6670
sprysiazhnyi <sprysiazhnyi@competo.io>
2024-11-19 20:22:09 +0200

    start

--- Project-21-Notification/ViewController.swift
+++ Project-21-Notification/ViewController.swift
@@ -6,14 +6,106 @@
 //
 
 import UIKit
+import UserNotifications
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UNUserNotificationCenterDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
+        
+        registerCategories()
     }
 
+    @objc func registerLocal() {
+        let center = UNUserNotificationCenter.current()
 
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if granted {
+                print("Yay!")
+            } else {
+                print("D'oh")
+            }
+        }
+    }
+
+    @objc func scheduleLocal() {
+        let center = UNUserNotificationCenter.current()
+
+        let content = UNMutableNotificationContent()
+        content.title = "Late wake up call"
+        content.body = "The early bird catches the worm, but the second mouse gets the cheese."
+        content.categoryIdentifier = "alarm"
+        content.userInfo = ["customData": "fizzbuzz"]
+        content.sound = UNNotificationSound.default
+
+        var dateComponents = DateComponents()
+        dateComponents.hour = 10
+        dateComponents.minute = 30
+       // let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
+    }
+    
+    func registerCategories() {
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+
+        let show = UNNotificationAction(identifier: "show", title: "Tell me more…", options: .foreground)
+        let category = UNNotificationCategory(identifier: "alarm", actions: [show], intentIdentifiers: [])
+
+        center.setNotificationCategories([category])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        // pull out the buried userInfo dictionary
+        let userInfo = response.notification.request.content.userInfo
+
+        if let customData = userInfo["customData"] as? String {
+            print("Custom data received: \(customData)")
+        }
+        // Создаем сообщение в зависимости от идентификатора действия
+            var alertTitle = ""
+            var alertMessage = ""
+
+            switch response.actionIdentifier {
+            case UNNotificationDefaultActionIdentifier:
+                // Пользователь просто нажал на уведомление
+                        alertTitle = "Default Action"
+                        alertMessage = "Вы просто нажали на уведомление."
+                
+                print("Default identifier")
+
+            case "show":
+                // Пользователь выбрал действие "Show more info"
+                       alertTitle = "Show More Info"
+                       alertMessage = "Вы выбрали действие 'Показать больше информации'."
+                
+                print("Show more information…")
+
+            default:
+                // Любое другое действие
+                     alertTitle = "Unknown Action"
+                     alertMessage = "Выбрано неизвестное действие."
+                break
+            }
+            
+        // Создаем и показываем UIAlertController
+        let alert = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default))
+        
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+           let rootViewController = windowScene.windows.first?.rootViewController {
+            rootViewController.present(alert, animated: true)
+        }
+
+
+        // you must call the completion handler when you're done
+        completionHandler()
+    }
 }
 
